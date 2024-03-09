31811fd35ee4dc791e079ac4c00e962cf1019af6
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-03-09 16:47:36 -0500

    split htc resources in different files

---
+++ modules/htc_res_defs/cluster.tf
@@ -0,0 +1,25 @@
+resource "humanitec_resource_definition" "k8s_cluster" {
+  driver_type = "humanitec/k8s-cluster-gke"
+  id          = "${var.prefix}cluster"
+  name        = "${var.prefix}cluster"
+  type        = "k8s-cluster"
+
+  driver_inputs = {
+    values_string = jsonencode({
+      "name"         = var.k8s_cluster_name
+      "loadbalancer" = var.k8s_loadbalancer
+      "project_id"   = var.k8s_project_id
+      "zone"         = var.k8s_region
+    }),
+    secrets_string = jsonencode({
+      "credentials" = var.k8s_credentials
+    })
+  }
+}
+
+resource "humanitec_resource_definition_criteria" "k8s_cluster" {
+  resource_definition_id = humanitec_resource_definition.k8s_cluster.id
+  env_id                 = var.environment
+  env_type               = var.environment_type
+
+}
---
+++ modules/htc_res_defs/namespace.tf
@@ -0,0 +1,32 @@
+resource "humanitec_resource_definition" "k8s_namespace" {
+  driver_type = "humanitec/template"
+  id          = "${var.prefix}namespace"
+  name        = "${var.prefix}namespace"
+  type        = "k8s-namespace"
+
+  driver_inputs = {
+    values_string = jsonencode({
+      templates = {
+        init      = "name: $${context.app.id}-$${context.env.id}"
+        manifests = <<EOL
+namespace.yaml:
+  location: cluster
+  data:
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      labels:
+        pod-security.kubernetes.io/enforce: restricted
+      name: {{ .init.name }}
+EOL
+        outputs   = "namespace: {{ .init.name }}"
+      }
+    })
+  }
+}
+
+resource "humanitec_resource_definition_criteria" "k8s_namespace" {
+  resource_definition_id = humanitec_resource_definition.k8s_namespace.id
+  env_id                 = var.environment
+  env_type               = var.environment_type
+}
