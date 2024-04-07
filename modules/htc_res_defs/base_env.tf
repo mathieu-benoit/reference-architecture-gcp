77e15f44037d33b5e2ffaf3e6537f07a6d4c1306
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-04-07 13:31:45 -0400

    base-env to test the TF runner

---
+++ modules/htc_res_defs/base_env.tf
@@ -0,0 +1,54 @@
+# This base-env is just to test the custom TF runner.
+resource "humanitec_resource_definition" "base_env" {
+  driver_type = "humanitec/terraform"
+  id          = "base-env"
+  name        = "base-env"
+  type        = "base-env"
+
+  driver_inputs = {
+    values_string = jsonencode({
+      append_logs_to_error = true
+      runner_mode          = "custom-kubernetes"
+      
+      runner = {
+        cluster_type = "gke"
+        cluster = {
+          region                   = "$${resources['config.default#terraform-runner'].outputs.zone}"
+          name                     = "$${resources['config.default#terraform-runner'].outputs.name}"
+          loadbalancer             = "$${resources['config.default#terraform-runner'].outputs.loadbalancer}"
+          project_id               = "$${resources['config.default#terraform-runner'].outputs.project_id}"
+        }
+        # FIXME - hard coded for now, needs to be passed as module's var.
+        service_account = "humanitec-terraform-runner"
+        # FIXME - hard coded for now, needs to be passed as module's var.
+        namespace       = "humanitec-terraform-runner"
+      }
+
+      script = <<EOL
+terraform {
+    backend "gcs" {
+        # FIXME - hard coded for now, needs to be passed as module's var.
+        bucket  = "htc-ref-arch-cluster-terraform-runner-state"
+    }
+}
+output "output" {
+    value = "simple-test-for-tf-runner"
+}
+EOL
+    })
+
+    secrets_string = jsonencode({
+      runner = {
+        credentials = "$${resources['config.default#terraform-runner'].outputs.credentials}"
+        agent_url   = "$${resources['agent.default#agent'].outputs.url}"
+      }
+    })
+  }
+}
+
+resource "humanitec_resource_definition_criteria" "base_env" {
+  resource_definition_id = humanitec_resource_definition.base_env.id
+  env_id                 = var.environment
+  env_type               = var.environment_type
+  force_delete           = true
+}
