bd4de3ace41643d0814b64cf236c0bbb5095ce1d
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-05-22 21:43:12 -0400

    make fmt

--- modules/htc_res_defs/base_env.tf
+++ modules/htc_res_defs/base_env.tf
@@ -12,11 +12,11 @@ resource "humanitec_resource_definition" "base_env" {
 
       runner = {
         cluster_type = "gke"
-        account = "${var.humanitec_org_id}/${humanitec_resource_account.cluster_account.id}"
+        account      = "${var.humanitec_org_id}/${humanitec_resource_account.cluster_account.id}"
         cluster = {
-          name         = var.k8s_cluster_name
-          project_id   = var.k8s_project_id
-          zone         = var.k8s_region
+          name       = var.k8s_cluster_name
+          project_id = var.k8s_project_id
+          zone       = var.k8s_region
         }
         # FIXME - hard coded for now, needs to be passed as module's var.
         service_account = "humanitec-terraform-runner"
