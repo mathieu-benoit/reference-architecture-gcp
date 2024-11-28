4526a33189c5bee295e22df906eba18c53d84397
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-11-28 14:43:55 -0500

    Register default SecretStore in Humanitec

--- modules/htc_res_defs/operator.tf
+++ modules/htc_res_defs/operator.tf
@@ -1,12 +1,20 @@
 locals {
   primary_secret_store = "primary"
+  default_secret_store = "default"
 }
 
 resource "humanitec_secretstore" "primary" {
   id      = local.primary_secret_store
   primary = true
   gcpsm = {
-    project_id = var.k8s_project_id
+    project_id = "not used is the orchestrator per se."
+  }
+}
+
+resource "humanitec_secretstore" "default" {
+  id      = local.default_secret_store
+  gcpsm = {
+    project_id = "not used is the orchestrator per se."
   }
 }
 
