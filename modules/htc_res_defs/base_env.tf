beb00a7241ec8e684e4e1b4f579b30482c072505
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-01-27 11:22:13 -0500

    externalize run-tofu.sh file

--- modules/htc_res_defs/base_env.tf
+++ modules/htc_res_defs/base_env.tf
@@ -32,54 +32,7 @@ resource "humanitec_resource_definition" "base_env" {
         # path = "echo"
       }
       files = {
-        "run.sh"                = <<END_OF_TEXT
-#!/bin/sh
-run_cmd ()
-{
-    if ! "$@" 2> "$\{ERROR_FILE}"
-    then
-        echo
-        echo "FAILED: $@"
-        cat "$\{ERROR_FILE}" 1>&2
-        exit 1
-    fi
-}
-if ! [ -d "$\{SCRIPTS_DIRECTORY}" ]
-then
-    echo "SCRIPTS_DIRECTORY does not exist: \"$\{SCRIPTS_DIRECTORY}"\" > "$\{ERROR_FILE}"
-    cat "$\{ERROR_FILE}" 1>&2
-    exit 1
-fi
-run_cmd cd "$\{SCRIPTS_DIRECTORY}"
-run_cmd ls -l
-if [ "$\{ACTION}" = "create" ]
-then
-    run_cmd tofu init -no-color
-    run_cmd tofu apply -auto-approve -input=false -no-color
-    mkdir output_parse_container
-    echo '{"in":' > output_parse_container/terraform.tfvars.json
-    run_cmd tofu output -json >> output_parse_container/terraform.tfvars.json
-    echo '}' >> output_parse_container/terraform.tfvars.json
-    run_cmd cd output_parse_container
-    echo 'variable "in" { type = map }
-output "values" { value = {for k, v in var.in: k => v.value if !v.sensitive} }
-output "secrets" { value = {for k, v in var.in: k => v.value if v.sensitive} }' > parse.tf
-    echo
-    echo "Converting outputs using tofu apply"
-    run_cmd tofu apply -auto-approve -input=false -no-color > /dev/null
-    run_cmd tofu output -json values > "$\{OUTPUTS_FILE}"
-    run_cmd tofu output -json secrets > "$\{SECRET_OUTPUTS_FILE}"
-    echo "Done."
-elif [ "$\{ACTION}" = "destroy" ]
-then
-    run_cmd tofu init -no-color
-    run_cmd tofu destroy -auto-approve -input=false -no-color
-else
-  echo "unrecognized ACTION: \"$\{ACTION}"\" > "$\{ERROR_FILE}"
-  cat "$\{ERROR_FILE}" 1>&2
-  exit 1
-fi
-END_OF_TEXT
+        "run.sh"                = file("${path.module}/scripts/run-tofu.sh")
         "terraform.tfvars.json" = "{\"input\": \"$${context.app.id}\"}\n"
         "backend.tf"            = <<END_OF_TEXT
 terraform {
---
+++ modules/htc_res_defs/scripts/run-tofu.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+run_cmd ()
+{
+    if ! "$@" 2> "$\{ERROR_FILE}"
+    then
+        echo
+        echo "FAILED: $@"
+        cat "$\{ERROR_FILE}" 1>&2
+        exit 1
+    fi
+}
+run_cmd cd $\{SCRIPTS_DIRECTORY}
+run_cmd ls -l
+run_cmd pwd
+if [ "$\{ACTION}" = "create" ]
+then
+    run_cmd tofu init -no-color
+    run_cmd tofu apply -auto-approve -input=false -no-color
+    mkdir output_parse_container
+    echo '{"in":' > output_parse_container/terraform.tfvars.json
+    run_cmd tofu output -json >> output_parse_container/terraform.tfvars.json
+    echo '}' >> output_parse_container/terraform.tfvars.json
+    run_cmd cd output_parse_container
+    echo 'variable "in" { type = map }
+output "values" { value = {for k, v in var.in: k => v.value if !v.sensitive} }
+output "secrets" { value = {for k, v in var.in: k => v.value if v.sensitive} }' > parse.tf
+    echo
+    echo "Converting outputs using tofu apply"
+    run_cmd tofu apply -auto-approve -input=false -no-color > /dev/null
+    run_cmd tofu output -json values > "$\{OUTPUTS_FILE}"
+    run_cmd tofu output -json secrets > "$\{SECRET_OUTPUTS_FILE}"
+    echo "Done."
+elif [ "$\{ACTION}" = "destroy" ]
+then
+    run_cmd tofu init -no-color
+    run_cmd tofu destroy -auto-approve -input=false -no-color
+else
+  echo "unrecognized ACTION: \"$\{ACTION}"\" > "$\{ERROR_FILE}"
+  cat "$\{ERROR_FILE}" 1>&2
+  exit 1
+fi
