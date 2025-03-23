45857c0ffd0cb0aad7b17a6c14166a19d687d437
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-03-23 14:54:19 -0400

    make backstage independant deployment

--- examples/with-backstage/main.tf
+++ examples/with-backstage/main.tf
@@ -1,23 +1,4 @@
-# GCP reference architecture
-
 locals {
-  repository_id   = "htc-ref-arch"
   repository_host = "${var.gar_repository_location}-docker.pkg.dev"
-  repository_name = "${local.repository_host}/${var.project_id}/${local.repository_id}"
-}
-
-module "base" {
-  source           = "../../modules/base"
-  project_id       = var.project_id
-  project_number   = var.project_number
-  region           = var.region
-  humanitec_org_id = var.humanitec_org_id
-  humanitec_prefix = var.humanitec_prefix
-  environment      = var.environment
-  environment_type = var.environment_type
-
-  gar_repository_id       = local.repository_id
-  gar_repository_location = var.gar_repository_location
-
-  gke_release_channel = var.gke_release_channel
+  repository_name = "${local.repository_host}/${var.project_id}/${var.gar_repository_id}"
 }
